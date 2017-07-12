d95e51ae25af1de839469e1aee096da4a0e1bb95
Domryuken <ladonjackson@hotmail.co.uk>
2017-07-12 16:34:23 +0100

    more stuff

--- JavaScriptBot/index.js
+++ JavaScriptBot/index.js
@@ -1,10 +1,11 @@
 const commando = require("discord.js-commando");
 const stuff = require('./JavaScriptBotStuff');
-const bot = new commando.Client();
+// const bot = new commando.Client();
+const bot = new commando.Client({ id:'id', commandPrefix:'{'});
 
 bot.registry.registerGroup("stuff", "Stuff")
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands")
 
 
-bot.login(stuff.key);
+bot.login(stuff.discord);
